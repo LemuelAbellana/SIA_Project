af792352ee7a26b3bbdc5df3b0c0469c833d9d49
SENGOMZ24 <labellana_230000002074@uic.edu.ph>
2024-11-14 19:32:42 +0800

    Delete dropdown_vs.js

--- dropdown_vs.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(button => {
-    button.addEventListener('click', () => {
-      const faqItem = button.parentElement;
-      faqItem.classList.toggle('active');
-      button.querySelector('.icon').textContent = faqItem.classList.contains('active') ? '-' : '+';
-    });
-  });
-  
